14836403a91a5e3fd899a288bd3182ec36f46a64
Nikita Spitsa <73286663+Nikspitsa@users.noreply.github.com>
2022-01-07 13:23:25 +0300

    Add files via upload

---
+++ HomeworkLec01Task01Ver01.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package by.jonline.lec01.v01;
+
+/**
+ * @author Nik
+ * task:
+ * calculate z=((a-3)*b/2)+c
+ */
+public class HomeworkLec01Task01Ver01 {
+	public static void main(String[] args) {
+		double a=2;
+		double b=2;
+		double c=3,z;
+		z=((a-3)*b/2)+c;
+		System.out.println(z);
+		
+	}
+
+}
