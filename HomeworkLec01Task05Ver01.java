17288166792d461ed350c1e2b95729ba7a1ca9ad
Nikita Spitsa <73286663+Nikspitsa@users.noreply.github.com>
2022-01-12 14:08:30 +0300

    Add files via upload

---
+++ HomeworkLec01Task05Ver01.java
@@ -0,0 +1,18 @@
+package by.jonline.lec01.v01;
+
+/*
+ * @author Nik
+ * Task
+ * here natural number T is number past time in seconds. Convert it to "ЧЧч MMмин ССс"
+ */
+public class HomeworkLec01Task05Ver01 {
+	public static void main (String[] args) {
+		int t=768908;
+		int a = t/3600;
+		int b = (t-a*3600)/60;
+		int c = t-a*3600-b*60;
+		System.out.println(a + "ч " + b + "мин " + c + "с " );
+		
+	}
+
+}
